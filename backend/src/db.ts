17bbc630e1728094f3c93d234121d1cbcecd34a3
Nino Akut <akutnino@gmail.com>
2024-11-23 05:02:34 +0800

    added logic to validate existing users in the auth rout

---
+++ backend/src/db.ts
@@ -0,0 +1,8 @@
+const Users = [
+	{
+		email: 'nino@gmail.com',
+		password: '123456789',
+	},
+];
+
+export default Users;
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,6 @@
 import express, { Router, Request, Response } from 'express';
 import { check, validationResult } from 'express-validator';
+import Users from '../db.js';
 
 const router: Router = express.Router();
 
@@ -12,7 +13,17 @@ router.post('/signup', baseAuthChain, (req: Request, res: Response) => {
 	const { password, email } = req.body;
 	const errors = validationResult(req);
 
+	// VALIDATE USER INPUT
 	if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
+
+	// VALIDATE IF USER EXIST
+	const userObject = Users.find((user) => user.email === email);
+
+	// prettier-ignore
+	if (userObject?.email) res.status(400).json({
+			errors: { msg: 'This user already exits.' },
+	});
+
 	res.send('Validation Passed');
 
 	console.log(password, email);
