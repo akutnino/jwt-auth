57d3607f474a0f54983750a557be83968ecb2dd9
Nino Akut <akutnino@gmail.com>
2024-11-24 05:44:39 +0800

    installed the jsonwebtoken package, created the token and sent it to the client

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,6 @@
 import express, { Router, Request, Response } from 'express';
 import { check, validationResult } from 'express-validator';
+import JWT from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import Users from '../db.js';
 
@@ -23,21 +24,33 @@ router.post('/signup', baseAuthChain, async (req: Request, res: Response) => {
 	const SALT_ROUNDS = 10;
 
 	// VALIDATE USER INPUT
-	if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
+	if (!errors.isEmpty()) {
+		res.status(400).json({ errors: errors.array() });
+		return;
+	}
 
 	// VALIDATE IF USER EXIST
 	const userObject: UserFindType = Users.find((user) => user.email === email);
 
-	// prettier-ignore
-	if (userObject?.email) res.status(400).json({
-			errors: { msg: 'This user already exits.' },
+	if (userObject?.email) {
+		res.status(400).json({ errors: { msg: 'This user already exits.' } });
+		return;
+	}
+
+	// HASH THE NEW USER PASSWORD
+	const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+	// INSERT THE NEW USER W/ HASHED PASSWORD TO DATABASE
+	Users.push({ email, password: hashedPassword });
+
+	// CREATE JWT
+	const PRIVATE_SECRET_KEY = 'vw45ghyth6uj678k90l3535ferr4cr';
+	const token = JWT.sign({ email }, PRIVATE_SECRET_KEY, {
+		expiresIn: 60000 * 60,
 	});
 
-	const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-	Users.push({ email, password: hashedPassword });
-	console.log(password, email);
-
-	res.send('Validation Passed');
+	// SEND THE TOKEN BACK TO THE CLIENT
+	res.send(token);
 });
 
 export { router as auth };
