a284b257854ab3f842510bd634344b680f136433
Nino Akut <akutnino@gmail.com>
2024-11-27 03:27:41 +0800

    added sample publicPosts and privatePosts datasets

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,5 +1,10 @@
 import { UserObect } from './routes/auth.js';
 
+type PostsObject = {
+	title: string;
+	content: string;
+};
+
 const Users: UserObect[] = [
 	{
 		email: 'nino@gmail.com',
@@ -7,4 +12,30 @@ const Users: UserObect[] = [
 	},
 ];
 
-export default Users;
+const PublicPosts: PostsObject[] = [
+	{
+		title: 'Free Tips on Development',
+		content: 'These are some tips',
+	},
+	{
+		title: 'Free Tips on Farmers',
+		content: 'These are some tips',
+	},
+	{
+		title: 'Free Tips on Students',
+		content: 'These are some tips',
+	},
+];
+
+const PrivatePosts: PostsObject[] = [
+	{
+		title: 'Paid Tips on Farmers',
+		content: 'These are some tips',
+	},
+	{
+		title: 'Paid Tips on Students',
+		content: 'These are some tips',
+	},
+];
+
+export { Users as default, PublicPosts, PrivatePosts };
